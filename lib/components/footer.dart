45ae02face926167fb995cba239fad7e089a4d4c
9-coding <9.coding.lab@gmail.com>
2023-04-03 01:49:24 +0900

    footer 적용

--- lib/components/footer.dart
+++ lib/components/footer.dart
@@ -1,27 +1,97 @@
-import 'package:aiia_webpage/components/globals.dart';
 import 'package:flutter/material.dart';
+
+//footer.dart
+double footerFontSize = 12; //footer 폰트 크기
+double footerHeight = 200; //footer 높이
 
 GlobalKey footerKey = GlobalKey();
 
 class Footer extends StatelessWidget {
-  const Footer({super.key});
+  const Footer({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-      padding: const EdgeInsets.all(7),
-      width: double.infinity,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(7), color: Colors.grey.shade300),
-      child: Text(
-        '가천대학교 글로벌 캠퍼스 교내 자율 동아리, AIIA입니다.\n'
-        '비즈니스 문의 : aiia.lab.official@gmail.com\n'
-        '© 2023 AIIA, All Rights Reserved.',
-        style: TextStyle(
-          fontSize: footerFontSize,
+    return Visibility(
+      visible: MediaQuery.of(context).size.height > footerHeight,
+      child: Container(
+        key: footerKey,
+        height: footerHeight,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: const Color(0xFF161C2A), // 검정색 배경색
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 2,
+              offset: const Offset(0, -2),
+            ),
+          ],
         ),
-        textAlign: TextAlign.end,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Image.asset(
+                  'assets/logos/white_logo.png', // 이미지 파일 경로
+                  width: 130, // 이미지 너비
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: const [
+                  Text(
+                    '경기도 성남시 수정구 성남대로 1324, AI공학관', // 주소 추가
+                    style: TextStyle(
+                      fontSize: 12.0,
+                      color: Colors.white, // 폰트 색상을 흰색으로 변경
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      'Phone: 010-9996-2407',
+                      style: TextStyle(
+                        fontSize: 12.0,
+                        color: Colors.white, // 폰트 색상을 흰색으로 변경
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      'Email: info@aiia.com',
+                      style: TextStyle(
+                        fontSize: 12.0,
+                        color: Colors.white, // 폰트 색상을 흰색으로 변경
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      '©2023 AIIA. All Rights Reserved.', // 저작권 문구 추가
+                      style: TextStyle(
+                        fontSize: 12.0,
+                        color: Colors.white, // 폰트 색상을 흰색으로 변경
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
